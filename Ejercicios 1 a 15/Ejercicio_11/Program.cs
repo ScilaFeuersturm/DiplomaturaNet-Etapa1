85dfb1eb43c92539475b56ac72362ecedfc1ac3e
Priscila Ayelen <priscilaayeleng@gmail.com>
2020-06-20 17:23:16 -0300

    Ejercicio 11

---
+++ Ejercicios 1 a 15/Ejercicio_11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese un número");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine($"El número ingresado tiene {checkLength(numero)} digitos");
+            Console.ReadKey();
+            
+        }
+        static int checkLength(int checkLength)
+        {
+            return checkLength.ToString().Length;
+        }
+    }
+}
